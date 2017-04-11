74beb77c5bf2cf737a01a023a6bab96fedee08d4
Anirudha Mahale <anirudhamahale@gmail.com>
2017-04-11 14:39:40 +0530

    automatically RTL is handled by apple.

---
+++ Localise-Demo/L102Language.swift
@@ -0,0 +1,15 @@
+//
+//  L102Language.swift
+//  Localise-Demo
+//
+//  Created by LA Argon on 11/04/17.
+//  Copyright © 2017 LA Argon. All rights reserved.
+//
+
+import UIKit
+
+// constants
+let APPLE_LANGUAGE_KEY = "AppleLanguages"
+class L102Language {
+    
+}
---
+++ Localise-Demo/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  Localise-Demo
+//
+//  Created by LA Argon on 11/04/17.
+//  Copyright © 2017 LA Argon. All rights reserved.
+//
+
+import UIKit
+
+class SecondView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+}
