b7ff23ab4286df95005fc1ea608240a30129932b
Anirudha Mahale <anirudhamahale@gmail.com>
2017-04-11 15:30:05 +0530

    Many things done.

---
+++ Localise-Demo/L012Localizer.swift
@@ -0,0 +1,89 @@
+//
+//  L012Localizer.swift
+//  Localise-Demo
+//
+//  Created by LA Argon on 11/04/17.
+//  Copyright © 2017 LA Argon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+extension UIApplication {
+    class func isRTL() -> Bool{
+        return UIApplication.shared.userInterfaceLayoutDirection == .rightToLeft
+    }
+}
+
+class L102Localizer: NSObject {
+    class func DoTheMagic() {
+        MethodSwizzleGivenClassName(cls: Bundle.self, originalSelector: #selector(Bundle.localizedString(forKey:value:table:)), overrideSelector: #selector(Bundle.specialLocalizedStringForKey(_:value:table:)))
+        MethodSwizzleGivenClassName(cls: UIApplication.self, originalSelector: #selector(getter: UIApplication.userInterfaceLayoutDirection), overrideSelector: #selector(getter: UIApplication.cstm_userInterfaceLayoutDirection))
+        MethodSwizzleGivenClassName(cls: UILabel.self, originalSelector: #selector(UILabel.layoutSubviews), overrideSelector: #selector(UILabel.cstmlayoutSubviews))
+    }
+}
+extension UILabel {
+    public func cstmlayoutSubviews() {
+        self.cstmlayoutSubviews()
+        if self.tag <= 0  {
+            if UIApplication.isRTL()  {
+                if self.textAlignment == .right {
+                    return
+                }
+            } else {
+                if self.textAlignment == .left {
+                    return
+                }
+            }
+        }
+        if self.tag <= 0 {
+            if UIApplication.isRTL()  {
+                self.textAlignment = .right
+            } else {
+                self.textAlignment = .left
+            }
+        }
+    }
+}
+extension UIApplication {
+    var cstm_userInterfaceLayoutDirection : UIUserInterfaceLayoutDirection {
+        get {
+            var direction = UIUserInterfaceLayoutDirection.leftToRight
+            if L102Language.currentAppleLanguage() == "ar" {
+                direction = .rightToLeft
+            }
+            return direction
+        }
+    }
+}
+extension Bundle {
+    func specialLocalizedStringForKey(_ key: String, value: String?, table tableName: String?) -> String {
+        if self == Bundle.main {
+            let currentLanguage = L102Language.currentAppleLanguage()
+            var bundle = Bundle();
+            if let _path = Bundle.main.path(forResource: currentLanguage, ofType: "lproj") {
+                bundle = Bundle(path: _path)!
+            } else {
+                let _path = Bundle.main.path(forResource: "Base", ofType: "lproj")!
+                bundle = Bundle(path: _path)!
+            }
+            return (bundle.specialLocalizedStringForKey(key, value: value, table: tableName))
+        } else {
+            return (self.specialLocalizedStringForKey(key, value: value, table: tableName))
+        }
+    }
+}
+func disableMethodSwizzling() {
+    
+}
+
+
+/// Exchange the implementation of two methods of the same Class
+func MethodSwizzleGivenClassName(cls: AnyClass, originalSelector: Selector, overrideSelector: Selector) {
+    let origMethod: Method = class_getInstanceMethod(cls, originalSelector);
+    let overrideMethod: Method = class_getInstanceMethod(cls, overrideSelector);
+    if (class_addMethod(cls, originalSelector, method_getImplementation(overrideMethod), method_getTypeEncoding(overrideMethod))) {
+        class_replaceMethod(cls, overrideSelector, method_getImplementation(origMethod), method_getTypeEncoding(origMethod));
+    } else {
+        method_exchangeImplementations(origMethod, overrideMethod);
+    }
+}
--- Localise-Demo/ViewController.swift
+++ Localise-Demo/ViewController.swift
@@ -21,6 +21,22 @@ class ViewController: UIViewController {
     }
     
     @IBAction func switchLanguage(_ sender: Any) {
+        print(L102Language.currentAppleLanguage())
+        if L102Language.currentAppleLanguage() == "en" {
+            L102Language.setAppleLAnguageTo(lang: "ar")
+            UIView.appearance().semanticContentAttribute = .forceRightToLeft
+        } else {
+            L102Language.setAppleLAnguageTo(lang: "en")
+            UIView.appearance().semanticContentAttribute = .forceLeftToRight
+        }
+        
+        let rootviewcontroller: UIWindow = ((UIApplication.shared.delegate?.window)!)!
+        rootviewcontroller.rootViewController = self.storyboard?.instantiateViewController(withIdentifier: "rootnav")
+        let mainwindow = (UIApplication.shared.delegate?.window!)!
+        mainwindow.backgroundColor = UIColor(hue: 0.6477, saturation: 0.6314, brightness: 0.6077, alpha: 0.8)
+        UIView.transition(with: mainwindow, duration: 0.55001, options: .transitionFlipFromLeft, animations: { () -> Void in
+        }) { (finished) -> Void in
+        }
         
     }
 
